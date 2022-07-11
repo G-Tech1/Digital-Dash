0e17ca429ae48c2b5cf14c0bb714539d44ea9e2b
kloudx15 <keenannguyen299@gmail.com>
2022-07-11 12:02:17 -0700

    started working on stuff

--- gigagraph/data_miner/models.py
+++ gigagraph/data_miner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DailyData(models.Model):
+    day = models.DateField(auto_now_add=True)
+    data_sent = models.FloatField()
+    data_received = models.FloatField()
+    data_total = models.FloatField()
+
+
+
--- gigagraph/gigagraph/settings.py
+++ gigagraph/gigagraph/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+    'data_miner.apps.DataMinerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Abstract User Model 
+
+AUTH_USER_MODEL = 'accounts.User'
