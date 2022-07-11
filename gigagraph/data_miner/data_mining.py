0dce593abd78f7eb63c519b89a0e575d67430ae4
kloudx15 <keenannguyen299@gmail.com>
2022-07-11 12:27:12 -0700

    data_mining.py

--- gigagraph/data_miner/data_mining.py
+++ gigagraph/data_miner/data_mining.py
@@ -6,6 +6,11 @@ last_received = psutil.net_io_counters().bytes_recv
 last_sent = psutil.net_io_counters().bytes_sent
 last_total = last_received + last_sent
 
+daily_received = 0
+daily_sent = 0
+daily_total = 0
+
+## While loop to track data being sent and received 
 
 while True:
     bytes_received = psutil.net_io_counters().bytes_recv
@@ -19,4 +24,18 @@ while True:
     mb_new_received = new_received / 1024 / 1024
     mb_new_sent = new_sent / 1024 / 1024
     mb_new_total = new_total / 1024 / 1024
+
+    print(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total")
+
+    last_received = bytes_received
+    last_sent = bytes_sent
+    last_total = bytes_total
+
+    ## added daily feature 
+    
+    daily_received += mb_new_received
+    daily_sent += mb_new_sent
+    daily_total = daily_sent + daily_received
+
+    time.sleep(1)
 
