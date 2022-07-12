1a3cf076f26006bba7d48af051ae43de5b4aa082
Gilan Serrant <gilserrant5@gmail.com>
2022-07-11 20:02:45 -0400

    edit data monitor

--- gigagraph/data_miner/data_mining.py
+++ gigagraph/data_miner/data_mining.py
@@ -1,16 +1,21 @@
 import time
 import psutil
 
-last_received = psutil.net_io_counters().bytes_recv
-last_sent = psutil.net_io_counters().bytes_sent
-last_total = last_received + last_sent
 
-session_received = 0
-session_sent = 0
-session_total = 0
 ## While loop to track data being sent and received 
-def data_monitor(true):
-    while true:
+def data_monitor():
+    
+    last_received = psutil.net_io_counters().bytes_recv
+    last_sent = psutil.net_io_counters().bytes_sent
+    last_total = last_received + last_sent
+
+    session_received = 0
+    session_sent = 0
+    session_total = 0
+
+    running = True
+
+    while running:
         bytes_received = psutil.net_io_counters().bytes_recv
         bytes_sent = psutil.net_io_counters().bytes_sent
         bytes_total = bytes_received + bytes_sent
@@ -23,7 +28,7 @@ def data_monitor(true):
         mb_new_sent = new_sent / 1024 / 1024
         mb_new_total = new_total / 1024 / 1024
 
-        print(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total, {session_total:.2f} MB daily total")
+        print(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total, {session_total:.2f} MB session total")
 
         last_received = bytes_received
         last_sent = bytes_sent
@@ -37,5 +42,9 @@ def data_monitor(true):
 
         time.sleep(1)
 
+    print [session_received, session_sent, session_total]
+    return [session_received, session_sent, session_total]
+
+data_monitor()
         # While False:
         #  print(f"{mb_new_received:.2f} MB received, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total, {session_total:.2f} MB daily total")
