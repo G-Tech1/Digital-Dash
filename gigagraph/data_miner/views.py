daf1c557688aa9838faa57b87a4276c7b7b50980
Gilan Serrant <gilserrant5@gmail.com>
2022-07-12 00:30:45 -0400

    created views for sessiondata

--- gigagraph/data_miner/views.py
+++ gigagraph/data_miner/views.py
@@ -3,12 +3,41 @@ from django.views import View
 from django.http import JsonResponse
 import json
 from .models import SessionData
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-class ListData(View):
-    def post(self, request, *args, **kwargs):
-        content = json.loads(self.request.body)
-        session = SessionData.objects.create(**content)
-        return JsonResponse(session, encoder=DataDetailEncoder, safe=False )
-    def get(self, request, *args, **kwargs): 
-        sessions = SessionData.objects.get()
+from .encoders import (
+    DataEncoder,
+)
+from .models import SessionData
+
+
+@require_http_methods(["GET", "POST"])
+def api_data(request):
+    if request.method == "GET":
+        data = SessionData.objects.all()
+        return JsonResponse(
+            {"Data": data},
+            encoder=DataEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        data = SessionData.objects.create(**content)
+        return JsonResponse(
+            data,
+            encoder=DataEncoder,
+            safe=False,
+        )
+
+
+
+# class ListData(View):
+#     def post(self, request, *args, **kwargs):
+#         content = json.loads(self.request.body)
+#         session = SessionData.objects.create(**content)
+#         return JsonResponse(session, encoder=DataDetailEncoder, safe=False )
+#     def get(self, request, *args, **kwargs): 
+#         sessions = SessionData.objects.get()
+
+
