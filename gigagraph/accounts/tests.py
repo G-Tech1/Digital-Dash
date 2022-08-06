349b36bd7a150bb9e54d0ef643a811baabe0ea47
kloudx15 <keenannguyen299@gmail.com>
2022-08-05 18:14:43 -0700

    merging

--- gigagraph/accounts/tests.py
+++ gigagraph/accounts/tests.py
@@ -1,27 +1,17 @@
 from django.test import TestCase
-import json
-from .views import create_user
+# from test_create_user import create_user
 
-## Keenan Nguyen Test Case
-class TestCreateUser(TestCase):
-    def test_create_user(self):
-        ex_json_content = {
-            "email": "FL@gmail.com",
-            "first_name": "First",
-            "last_name": "Last",
-            "username": "exampleuser",
-            "password": "",
-            }
 
-        result = create_user(ex_json_content)
-
-        expected = "missing properties"
-        # expected = {
-        #     "email": "FL@gmail.com",
-        #     "first_name": "First",
-        #     "last_name": "Last",
-        #     "username": "exampleuser",
-        #     "password": "PSut19*@",
-        #     }
-        
-        self.assertEqual(result, expected)
+# ## Keenan Nguyen Test Case
+# class TestCreateUser(TestCase):
+#     def test_create_user(self):
+#         input = {
+#             "email": "FL@gmail.com",
+#             "first_name": "First",
+#             "last_name": "Last",
+#             "username": "username",
+#             "password": "password",
+#         }
+#         result = create_user(input)
+#         expected = 200
+#         self.assertEqual(result, expected)
