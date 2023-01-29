9b5021dc61379c00c858e6a2257e8e57d5fae9e1
Gilan <gilserrant5@gmail.com>
2023-01-29 15:34:40 -0500

    runtime file added

--- gigagraph/gigagraph/settings.py
+++ gigagraph/gigagraph/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-8wee@f*t)ui%-9ezx(h%*f31+^x!*qq^0j)dq^!b2!4x3r12dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3001",
